8033da8b0718f5602eeb0e490866527a55c32299
ngocthinh09 <volengocthinh09@gmail.com>
2026-03-28 03:48:57 +0000

    feat(train): implement learning rate scheduling with cosine decay and linear warmup

--- train.py
+++ train.py
@@ -5,6 +5,26 @@ from config import GPTConfig
 from model.transformer import GPT
 from data.loader import DataLoaderLite
 import time
+import math
+
+max_lr = 3e-4
+min_lr = max_lr * 0.1
+warmup_steps = 10
+max_steps = 100
+
+def get_lr(it):
+    # 1. Linear Warmup from 0 to max_lr for the first warmup_steps
+    if (it < warmup_steps):
+        return max_lr * (it + 1) / warmup_steps
+    # 2. If it > warmup_steps, then decay it with cosine decay down to min_lr over the course of max_steps
+    if (it > max_steps):
+        return min_lr
+    # 3. In between, use cosine decay from max_lr to min_lr
+    decay_ratio = (it - warmup_steps) / (max_steps - warmup_steps)
+    assert 0 <= decay_ratio <= 1, "Decay ratio should be between 0 and 1"
+    coef = 0.5 * (1.0 + math.cos(math.pi * decay_ratio))
+    return min_lr + coef * (max_lr - min_lr)
+
 
 if __name__ == "__main__":
     device = "cpu"
@@ -26,7 +46,7 @@ if __name__ == "__main__":
     model = torch.compile(model)
     
     optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4, betas=(0.9, 0.95), eps=1e-8)
-    for i in range(100):
+    for step in range(max_steps):
         t0 = time.time()
         x, y = train_loader.next_batch()
         x, y = x.to(device), y.to(device)
@@ -35,9 +55,12 @@ if __name__ == "__main__":
             logits, loss = model(x, y)
         loss.backward()
         norm = torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
+        lr = get_lr(step)
+        for param_group in optimizer.param_groups:
+            param_group['lr'] = lr
         optimizer.step()
         torch.cuda.synchronize() if torch.cuda.is_available() else None
         t1 = time.time()
         dt = (t1 - t0) * 1000
         tokens_per_sec = train_loader.B * train_loader.T / (t1 - t0)
-        print(f"Step {i}| Loss {loss.item()}| Grad Norm {norm.item():.2f}| Time: {dt:.2f} ms| Tokens/sec: {tokens_per_sec:.2f}")
+        print(f"Step: {step}| Loss: {loss.item()}| LR: {lr:.2e}| Grad Norm: {norm.item():.2f}| Time: {dt:.2f} ms| Tokens/sec: {tokens_per_sec:.2f}")
